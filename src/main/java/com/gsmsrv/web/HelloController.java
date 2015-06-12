36adeb57215ff49820e514d996c802f8389aeebe
bobcat13 <bob.cat13@yandex.ru>
2015-06-12 18:21:54 +0300

    Change project main name to gsmsrv.

--- src/main/java/com/gsmsrv/web/HelloController.java
+++ src/main/java/com/gsmsrv/web/HelloController.java
@@ -1,4 +1,4 @@
-package com.springapp.mvc;
+package com.gsmsrv.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
--- src/test/java/com/gsmsrv/web/AppTests.java
+++ src/test/java/com/gsmsrv/web/AppTests.java
@@ -1,4 +1,4 @@
-package com.springapp.mvc;
+package com.gsmsrv.web;
 
 import org.junit.Before;
 import org.junit.Test;
